664dcb5411414c7988539bcb8c2c47645c18eef7
Sasa33 <chengsasa33@gmail.com>
2017-11-08 21:08:53 +0800

    add nearby cats list item

--- CatKeeper/app/src/main/java/com/tw/training/catkeeper/adapter/NearbyCatAdapter.kt
+++ CatKeeper/app/src/main/java/com/tw/training/catkeeper/adapter/NearbyCatAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.tw.training.catkeeper.R
@@ -22,9 +23,9 @@ class NearbyCatAdapter(val context: Context, val data: List<String>) : BaseAdapt
         } else {
             view = convertView
         }
-        var textView = view.findViewById<TextView>(R.id.content)
-        textView.text = data[position]
-        textView.setOnClickListener{
+        var content = view.findViewById<RelativeLayout>(R.id.content)
+//        content.text = data[position]
+        content.setOnClickListener{
             Toast.makeText(context, data[position], Toast.LENGTH_SHORT).show()
         }
         return view
